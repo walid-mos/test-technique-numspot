6f323efa679eb88447eaa83d6718c3dff3a0695e
Walid Mostefaoui <pro.walid.mostefaoui@gmail.com>
2024-02-15 13:06:36 +0100

    feat: added shadcnui

---
+++ lib/functions/classnames.ts
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
